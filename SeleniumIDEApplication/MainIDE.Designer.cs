86616cbf0b7901e037a93a417395c89e4dc56f0e
Mai Thanh <mai_bom2002@yahoo.com>
2017-09-28 09:52:12 +0700

    Add project files.

---
+++ SeleniumIDEApplication/MainIDE.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SeleniumIDEApplication
+{
+    partial class MainIDE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.topMain = new System.Windows.Forms.Splitter();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitter2);
+            this.splitContainer1.Panel1.Controls.Add(this.topMain);
+            this.splitContainer1.Size = new System.Drawing.Size(826, 475);
+            this.splitContainer1.SplitterDistance = 361;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // topMain
+            // 
+            this.topMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topMain.Location = new System.Drawing.Point(0, 0);
+            this.topMain.Name = "topMain";
+            this.topMain.Size = new System.Drawing.Size(826, 82);
+            this.topMain.TabIndex = 0;
+            this.topMain.TabStop = false;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(0, 82);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(275, 279);
+            this.splitter2.TabIndex = 1;
+            this.splitter2.TabStop = false;
+            // 
+            // MainIDE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 475);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainIDE";
+            this.Text = "Seleium IDE";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Splitter topMain;
+    }
+}
+
